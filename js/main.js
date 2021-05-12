3e8901ef5ffee7ddf65eabfe6df2628657651013
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-05-12 15:38:51 +0300

    mobile first

--- js/main.js
+++ js/main.js
@@ -5,32 +5,28 @@ $('.top-slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
+  nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next.svg" alt="Next arrow"></button>',
+  prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev.svg" alt="Prev arrow"></button>',
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+        
       }
     },
     {
       breakpoint: 640,
       settings: {
-        arrows: false,
+       arrows: false,
       }
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
         arrows: false,
+        
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
+    
   ]
 });
