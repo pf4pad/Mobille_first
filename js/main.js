44b788dac9de43c5701b561c0384172c0688f2ab
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-05-12 13:18:28 +0300

    mobile first

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-$(function(){
-
+$('.top-slider').slick({
+  dots: true,
+  speed: 500,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        arrows: false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
